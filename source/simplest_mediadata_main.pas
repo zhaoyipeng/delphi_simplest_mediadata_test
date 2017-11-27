ecff5981a62e3bdbc97cab5c1f7cdf2dcef9c4d8
zhaoyipeng <zhaoyipeng@hotmail.com>
2017-11-27 13:40:50 +0800

    finished simplest_mediadata_raw unit

--- source/simplest_mediadata_main.pas
+++ source/simplest_mediadata_main.pas
@@ -11,15 +11,17 @@ implementation
 procedure test_all;
 begin
   //Test
-  simplest_yuv420_split('lena_256x256_yuv420p.yuv',256,256,1);
-  simplest_yuv444_split('lena_256x256_yuv444p.yuv',256,256,1);
-  simplest_yuv420_gray('lena_256x256_yuv420p.yuv',256,256,1);
-  simplest_yuv420_halfy('lena_256x256_yuv420p.yuv',256,256,1);
-  simplest_yuv420_graybar(640, 360,0,255,10,'graybar_640x360.yuv');
-  simplest_yuv420_psnr('lena_256x256_yuv420p.yuv','lena_distort_256x256_yuv420p.yuv',256,256,1);
-  simplest_rgb24_split('cie1931_500x500.rgb', 500, 500,1);
-  simplest_rgb24_to_bmp('lena_256x256_rgb24.rgb',256,256,'output_lena.bmp');
+//  simplest_yuv420_split('lena_256x256_yuv420p.yuv',256,256,1);
+//  simplest_yuv444_split('lena_256x256_yuv444p.yuv',256,256,1);
+//  simplest_yuv420_gray('lena_256x256_yuv420p.yuv',256,256,1);
+//  simplest_yuv420_halfy('lena_256x256_yuv420p.yuv',256,256,1);
+//  simplest_yuv420_graybar(640, 360,0,255,10,'graybar_640x360.yuv');
+//  simplest_yuv420_psnr('lena_256x256_yuv420p.yuv','lena_distort_256x256_yuv420p.yuv',256,256,1);
+//  simplest_rgb24_split('cie1931_500x500.rgb', 500, 500,1);
+//  simplest_rgb24_to_bmp('lena_256x256_rgb24.rgb',256,256,'output_lena.bmp');
+//  simplest_rgb24_to_yuv420('lena_256x256_rgb24.rgb',256,256,1,'output_lena.yuv');
 
+  simplest_rgb24_colorbar(640, 360,'colorbar_640x360.rgb');
 
 
   Readln;
--- source/simplest_mediadata_raw.pas
+++ source/simplest_mediadata_raw.pas
@@ -61,7 +61,7 @@ function simplest_yuv420_border(const url: string; w, h, border, num: Integer): 
 
 
 /// <summary>
-/// Add border for YUV420P file
+///  Generate YUV420P gray scale bar.
 /// </summary>
 /// <param name="width">Width of Output YUV file.</param>
 /// <param name="height">Height of Output YUV file.</param>
@@ -97,8 +97,26 @@ function simplest_rgb24_split(const url: string; w, h, num: Integer): Integer;
 /// <param name="rgb24path">Location of Input RGB file.</param>
 /// <param name="width">Width of Input RGB file.</param>
 /// <param name="height">Height of Input RGB file.</param>
-/// <param name="bmppath">Location of Output BMP file. </param>
+/// <param name="bmppath">Location of Output BMP file.</param>
 function simplest_rgb24_to_bmp(const rgb24path: string; width, height: Integer; const bmppath: string): Integer;
+
+/// <summary>
+/// Convert RGB24 file to YUV420P file
+/// </summary>
+/// <param name="url_in">Location of Input RGB file.</param>
+/// <param name="w">Width of Input RGB file.</param>
+/// <param name="h">Height of Input RGB file.</param>
+/// <param name="num">Number of frames to process.</param>
+/// <param name="url_out">Location of Output YUV file.</param>
+function simplest_rgb24_to_yuv420(const url_in: string; w, h, num: Integer; const url_out: string): Integer;
+
+/// <summary>
+///  Generate RGB24 colorbar.
+/// </summary>
+/// <param name="width">Width of Output RGB file.</param>
+/// <param name="height">Height of Output RGB file.</param>
+/// <param name="url_out">Location of Output RGB file.</param>
+function simplest_rgb24_colorbar(width, height: Integer; const url_out: string): Integer;
 
 implementation
 
@@ -268,9 +286,6 @@ var
   data_y, data_u, data_v: TArray<Byte>;
   t, i, j: Integer;
 begin
-  t := 0;
-  i := 0;
-  j := 0;
   barwidth := width div barnum;
   lum_inc := (ymax - ymin) / (barnum - 1);
   uv_width := width div 2;
@@ -327,7 +342,6 @@ var
   mse_sum, mse, psnr: Double;
   I: Integer;
   J: Integer;
-  K: Integer;
 begin
   fp1 := TFileStream.Create(url1, fmOpenRead);
   fp2 := TFileStream.Create(url2, fmOpenRead);
@@ -336,8 +350,6 @@ begin
   SetLength(pic2, w * h);
 
   mse_sum := 0;
-  mse := 0;
-  psnr := 0;
   for I := 0 to num - 1 do
   begin
     fp1.ReadBuffer(pic1[0], w * h);
@@ -450,6 +462,163 @@ begin
   fp_bmp.Free;
   fp_rgb24.Free;
   Writeln(Format('Finish generate %s', [bmppath]));
+  Result := 0;
+end;
+
+function clip_value(x, min_val, max_val: Byte): Byte;
+begin
+  if (x > max_val) then
+    Exit(max_val)
+  else if (x < min_val) then
+    Exit(min_val)
+  else
+    Exit(x);
+end;
+
+//RGB to YUV420
+function RGB24_TO_YUV420(RgbBuf: PByte; w, h: Integer; yuvBuf: PByte): Boolean;
+var
+  ptrY, ptrU, ptrV, ptrRGB: PByte;
+  J: Integer;
+  I: Integer;
+  y, u, v, r, g, b: Byte;
+begin
+  FillChar(yuvBuf^, w * h * 3 div 2, 0);
+  ptrY := yuvBuf;
+  ptrU := yuvBuf + w * h;
+  ptrV := ptrU + w * h div 4;
+  for J := 0 to h - 1 do
+  begin
+    ptrRGB := RgbBuf + w * J * 3;
+    for I := 0 to w - 1 do
+    begin
+      r := ptrRGB^;
+      Inc(ptrRGB);
+      g := ptrRGB^;
+      Inc(ptrRGB);
+      b := ptrRGB^;
+      Inc(ptrRGB);
+      y := ((66 * r + 129 * g + 25 * b + 128) shr 8) + 16;
+      u := ((-38 * r - 74 * g + 112 * b + 128) shr 8) + 128;
+      v := ((112 * r - 94 * g - 18 * b + 128) shr 8) + 128;
+
+      ptrY^ := clip_value(y, 0, 255);
+      Inc(ptrY);
+      if ((J mod 2) = 0) and ((I mod 2) = 0) then
+      begin
+        ptrU^ := clip_value(u, 0, 255);
+        Inc(ptrU);
+      end
+      else
+      begin
+        if (I mod 2 = 0) then
+        begin
+          ptrV^ := clip_value(v, 0, 255);
+          Inc(ptrV);
+        end;
+      end;
+    end;
+  end;
+  Result := True;
+end;
+
+function simplest_rgb24_to_yuv420(const url_in: string; w, h, num: Integer; const url_out: string): Integer;
+var
+  fp, fp1: TFileStream;
+  pic_rgb24, pic_yuv420: TArray<Byte>;
+  I: Integer;
+begin
+  fp := TFileStream.Create(url_in, fmOpenRead);
+  fp1 := TFileStream.Create(OUTPUT_DIR + url_out, fmCreate);
+
+  SetLength(pic_rgb24, w * h * 3);
+  SetLength(pic_yuv420, w * h * 3 div 2);
+
+  for I := 0 to num - 1 do
+  begin
+    fp.ReadBuffer(pic_rgb24[0], w * h * 3);
+    RGB24_TO_YUV420(@pic_rgb24[0], w, h, @pic_yuv420[0]);
+    fp1.WriteBuffer(pic_yuv420[0], w * h * 3 div 2);
+  end;
+
+  fp1.Free;
+  fp.Free;
+
+  Result := 0;
+end;
+
+function simplest_rgb24_colorbar(width, height: Integer; const url_out: string): Integer;
+var
+  data: TArray<Byte>;
+  barwidth: Integer;
+  filename: string;
+  fp: TFileStream;
+  i, j, barnum: Integer;
+begin
+  SetLength(data, width * height * 3);
+  barwidth := width div 8;
+
+  fp := TFileStream.Create(OUTPUT_DIR + url_out, fmCreate);
+
+  for j := 0 to height - 1 do
+  begin
+    for i := 0 to width - 1 do
+    begin
+      barnum := i div barwidth;
+      case barnum of
+        0:
+          begin
+            data[(j * width + i) * 3 + 0] := 255;
+            data[(j * width + i) * 3 + 1] := 255;
+            data[(j * width + i) * 3 + 2] := 255;
+          end;
+        1:
+          begin
+            data[(j * width + i) * 3 + 0] := 255;
+            data[(j * width + i) * 3 + 1] := 255;
+            data[(j * width + i) * 3 + 2] := 0;
+          end;
+        2:
+          begin
+            data[(j * width + i) * 3 + 0] := 0;
+            data[(j * width + i) * 3 + 1] := 255;
+            data[(j * width + i) * 3 + 2] := 255;
+          end;
+        3:
+          begin
+            data[(j * width + i) * 3 + 0] := 0;
+            data[(j * width + i) * 3 + 1] := 255;
+            data[(j * width + i) * 3 + 2] := 0;
+          end;
+        4:
+          begin
+            data[(j * width + i) * 3 + 0] := 255;
+            data[(j * width + i) * 3 + 1] := 0;
+            data[(j * width + i) * 3 + 2] := 255;
+          end;
+        5:
+          begin
+            data[(j * width + i) * 3 + 0] := 255;
+            data[(j * width + i) * 3 + 1] := 0;
+            data[(j * width + i) * 3 + 2] := 0;
+          end;
+        6:
+          begin
+            data[(j * width + i) * 3 + 0] := 0;
+            data[(j * width + i) * 3 + 1] := 0;
+            data[(j * width + i) * 3 + 2] := 255;
+          end;
+        7:
+          begin
+            data[(j * width + i) * 3 + 0] := 0;
+            data[(j * width + i) * 3 + 1] := 0;
+            data[(j * width + i) * 3 + 2] := 0;
+          end;
+      end;
+    end;
+  end;
+  fp.WriteBuffer(data[0], width * height * 3);
+  fp.Free;
   Result := 0;
 end;
 
